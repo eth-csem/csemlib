8acd391c6ac5ff88dd4a7586483c9d95c46b6a30
echolite <andreas.fichtner@erdw.ethz.ch>
2017-02-07 11:29:25 +0100

    Implement reader of grid data, readers.read_from_grid.

--- csemlib/io/readers.py
+++ csemlib/io/readers.py
@@ -1,1 +1,23 @@
-print "Hello"
+
+def read_from_grid(filename):
+    """
+    Read cartesian grid points from a file named "filename", organised as
+    x1, y1, z1
+    x2, y2, z2
+    ... .
+    """
+
+    x=[]
+    y=[]
+    z=[]
+
+    fid=open(filename,'r')
+
+    for point in fid:
+        x.append(float(point.split()[0]))
+        y.append(float(point.split()[1]))
+        z.append(float(point.split()[2]))
+
+    fid.close()
+
+    return x, y, z
